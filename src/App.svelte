ace817be7a7339ec2bfce0882cdab7fc94ed020f
AntonBrito <tonybrito85@gmail.com>
2021-06-13 17:46:44 -0400

    Added fuctionality to depth var

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,22 @@
 	let name = "Anton";
 	let locationName = "KeyWest FL";
 	let depth = 30;
+
+	const addDepth = () => (depth += 1);
+	const removeDepth = () => (depth -= 1);
+	
 </script>
-<div class="card">
-	<h1>{name}</h1>
-	<h3>Location:{locationName}</h3>
-	<h3>Depth:{depth}Ft</h3>
+<div class="container">
+	<div class="card">
+		<h1>{name}</h1>
+		<h3>Location:{locationName}</h3>
+		<h3>Depth:{depth}Ft</h3>
+		<button class="btn" on:click={addDepth}>+1</button>
+		<button class="btn btn-dark" on:click={removeDepth}>-1</button>
+	</div>
 </div>
 <style>
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		color: #333333;
 	}
-
 </style>
