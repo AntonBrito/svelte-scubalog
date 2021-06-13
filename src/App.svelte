3e08a7493ffec3d7d42e3f57f85653b83c3fa71f
AntonBrito <tonybrito85@gmail.com>
2021-06-13 17:32:04 -0400

    Added card to the layout

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
 	let locationName = "KeyWest FL";
 	let depth = 30;
 </script>
+<div class="card">
 	<h1>{name}</h1>
 	<h3>Location:{locationName}</h3>
 	<h3>Depth:{depth}Ft</h3>
+</div>
 <style>
 	h1 {
 		color: #ff3e00;
