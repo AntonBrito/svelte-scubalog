1c367f32bbd5452b54dbc1ec2e503dc541be6c29
AntonBrito <tonybrito85@gmail.com>
2021-06-13 18:22:16 -0400

    Added button fuctionality for toggling the controls

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,23 @@
 	let name = "Anton";
 	let locationName = "KeyWest FL";
 	let depths = 30;
+	let showControls = false
 
 	const addDepth = () => (depths += 1);
 	const removeDepth = () => (depths -= 1);
-
+	const toggleControls = () => (showControls = !showControls); 
 </script>
 <div class="container">
 	<div class="card">
 		<h1>{name}</h1>
+		<button class="btn btn-sm" on:click={toggleControls}>+</button> 
 		<h3>Location:{locationName}</h3>
 		<h3>Depth:{depths}Ft</h3>
+		{#if showControls}
 		<button class="btn" on:click={addDepth}>+1</button>
 		<button class="btn btn-dark" on:click={removeDepth}>-1</button>
 		<input type="number" bind:value={depths} />
+		{/if}
 	</div>
 </div>
 <style>
