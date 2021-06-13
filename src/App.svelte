98e29a1470c867c252b925ee2301241fea0f26e0
AntonBrito <tonybrito85@gmail.com>
2021-06-13 19:16:32 -0400

    Created a variable of divers

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script>
 	import Navbar from "./Navbar.svelte";
-	import Divers from "./Divers.svelte";
+	import Divers from "./Diver.svelte";
 
+	let divers = [
+		{
+			name: "Anton",
+			location: "KeyWest, FL",
+			depth: 12,
+		},
+		{
+			name: "Riley",
+			location: "Jamestown, RI",
+			depth: 20,
+		},
+		{
+			name: "Belmas",
+			location: "QuebraCanela, CapeVerde",
+			depth: 12,
+		},
+
+	];
 </script>
 <Navbar />
 <div class="container">
-	
+	{#if divers.length === 0}
+		<p>No Dives Today</p>
+		{:else}
+			{#each divers as diver}
+				<Divers />
+			{/each}
+	{/if}
 </div>
