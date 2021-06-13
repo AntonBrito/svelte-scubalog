530b2128b8f705d13fe3b757bd5c665f086ca025
AntonBrito <tonybrito85@gmail.com>
2021-06-13 17:57:42 -0400

    Fixed a typo on depths, added depths input field

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,20 @@
 <script>
 	let name = "Anton";
 	let locationName = "KeyWest FL";
-	let depth = 30;
+	let depths = 30;
 
-	const addDepth = () => (depth += 1);
-	const removeDepth = () => (depth -= 1);
-	
+	const addDepth = () => (depths += 1);
+	const removeDepth = () => (depths -= 1);
+
 </script>
 <div class="container">
 	<div class="card">
 		<h1>{name}</h1>
 		<h3>Location:{locationName}</h3>
-		<h3>Depth:{depth}Ft</h3>
+		<h3>Depth:{depths}Ft</h3>
 		<button class="btn" on:click={addDepth}>+1</button>
 		<button class="btn btn-dark" on:click={removeDepth}>-1</button>
+		<input type="number" bind:value={depths} />
 	</div>
 </div>
 <style>
